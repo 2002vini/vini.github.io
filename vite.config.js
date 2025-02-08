e70849e76a62bfb96eb9593e914b0d8cece3a669
Vini Hundlani <hundlanivini2002@gmail.com>
2025-02-09 01:32:51 +0530

    Fix 404 issue on GitHub Pages by changing base in vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/2002vini.github.io/vini.github.io/",
+  base:"/vini.github.io/",
   plugins: [react()],
   css: {
     modules: {
