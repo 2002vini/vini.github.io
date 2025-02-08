e43e4ebe413d15192bec0f64d167940c2993c5b8
Vini Hundlani <hundlanivini2002@gmail.com>
2025-02-09 01:27:15 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/vini.github.io/",
+  base:"/2002vini.github.io/vini.github.io/",
   plugins: [react()],
   css: {
     modules: {
